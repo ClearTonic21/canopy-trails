22122e63958788217d3fed3fb0cfa40828911ddb
ClearTonic Games_ <cleartonicgames@gmail.com>
2025-09-15 15:14:22 -0400

    overview-header and icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OverviewHeader } from "./components/overview-header/overview-header";
+import { ViewPort } from "./components/view-port/view-port";
+import { UnderviewFooter } from "./components/underview-footer/underview-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OverviewHeader, ViewPort, UnderviewFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('CANOPY TRAILS');
+  protected readonly standardTitle = signal<string>('CANOPY TRAILS');
+  userTitle = signal<string>('MY CANOPY TRAIL');
 }
--- src/app/components/overview-header/overview-header.ts
+++ src/app/components/overview-header/overview-header.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'overview-header',
-  imports: [],
+  imports: [ MatIconModule, MatToolbarModule, MatButtonModule ],
   templateUrl: './overview-header.html',
   styleUrl: './overview-header.scss'
 })
 export class OverviewHeader {
-
+  @Input() userTitle = signal<string>( 'User Title' );
 }
