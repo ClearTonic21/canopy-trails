f96bac30f0c22f30b61ecfc3910d280a3bbecaee
ClearTonic Games_ <cleartonicgames@gmail.com>
2025-09-16 12:09:31 -0400

    initial styling and layout of components

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { OverviewHeader } from "./components/overview-header/overview-header";
 import { ViewPort } from "./components/view-port/view-port";
 import { UnderviewFooter } from "./components/underview-footer/underview-footer";
+import { TabPanel } from './components/tab-panel/tab-panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OverviewHeader, ViewPort, UnderviewFooter],
+  imports: [RouterOutlet, OverviewHeader, ViewPort, TabPanel, UnderviewFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/view-port/view-port.ts
+++ src/app/components/view-port/view-port.ts
@@ -1,11 +1,10 @@
 import { Component, Input, signal } from '@angular/core';
 import { SelectorPanel } from "./components/selector-panel/selector-panel";
 import { ControlPanel } from "./components/control-panel/control-panel";
-import { TabPanel } from "./components/tab-panel/tab-panel";
 
 @Component({
   selector: 'view-port',
-  imports: [SelectorPanel, ControlPanel, TabPanel],
+  imports: [SelectorPanel, ControlPanel],
   templateUrl: './view-port.html',
   styleUrl: './view-port.scss'
 })
