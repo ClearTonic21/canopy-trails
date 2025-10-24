ee69beb7560969d3932ed6c2ea33a139d357167c
ClearTonic Games_ <cleartonicgames@gmail.com>
2025-10-23 22:52:51 -0400

    new branch component

---
+++ src/app/components/trail/components/tree/components/branch/branch.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'branch',
+  imports: [],
+  templateUrl: './branch.html',
+  styleUrl: './branch.scss'
+})
+export class Branch {
+
+}
