25e6bc8cd8bb5ae8e3000444d6b4d0dbf68debff
ClearTonic Games_ <cleartonicgames@gmail.com>
2025-10-28 12:49:59 -0400

    index.js base finished

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,4 +8,11 @@ const ports = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'Content-Type, Authorization');
+  next();
+})
+
 app.listen(ports, () => console.log(`Listening on port ${ports}`));
