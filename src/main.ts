abc6f4f19f2fd39ef0afc2e2d2549f6443036ab2
ClearTonic Games_ <cleartonicgames@gmail.com>
2025-11-06 15:17:30 -0500

    arranged for backend/frontend

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { appConfig } from './frontend/app/app.config';
+import { App } from './frontend/app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
